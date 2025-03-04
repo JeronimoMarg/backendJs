8eda189c55739df7a503f2b00caaf056534460d5
JeronimoMarg <jeronimomargitic@gmail.com>
2025-03-04 20:19:04 -0300

    Union de front con back

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 const express = require('express')
 const app = express()
-
 app.use(express.json())
+
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.static('dist'))
 
 let notes = [
   {
@@ -78,23 +82,12 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
