92ee2d2d1f715225532671d089992d8cb0af6635
JeronimoMarg <jeronimomargitic@gmail.com>
2025-03-06 23:49:29 -0300

    backend con mongoDB

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const Note = require('./models/note')
 
 const express = require('express')
 const app = express()
@@ -8,28 +10,7 @@ app.use(cors())
 
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  },
-  {
-    id: "4",
-    content: "Otra Nota mas",
-    important: true
-  }
-]
+//---------------------------------------------------------------------------------------------//
 
 //Pagina de bienvenida?
 app.get('/', (request, response) => {
@@ -38,53 +19,81 @@ app.get('/', (request, response) => {
   
 //Obtener todas las notas
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+  Note.find({}).then(n => {
+    response.json(n)
+  })
 })
 
 //Obtener una nota especifica (id)
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if(note){
+    Note.findById(id).then(note => {
+      if(note){
         response.json(note)
-    }else{
-        response.status(404).end()
-    }
+      }else{
+        response.status(404).send({error: 'note not found'})
+      }
+    }).catch(error => next(error))
 })
 
 //Borrar una nota por id
 app.delete('/api/notes/:id', (request, response) => {
     const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+    Note.findByIdAndDelete(id).then(() => {
+      //En este caso no distingo entre encontrada y no para no informar si existia en base de datos
+      response.status(204).end()
+    }).catch(error => {
+      console.log(error)
+      response.status(500).send({error: "Server error"})
+    })
 })
 
+/* //Funcion antigua para generar IDs de objetos
 const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => Number(n.id)))
     : 0
   return String(maxId + 1)
 }
+*/
 
 //Crear una nueva nota
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({ error: 'content missing'})
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    id: generateId(),
+    important: body.important || false
+  })
+
+  note.save().then(result => {
+    console.log('note saved!', result)
+    response.json(result)
+  })
+})
+
+//Update de la nota
+app.put('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
+  const body = request.body
+  if(!body.content){
+      return response.status(400).json({
+          error: 'content missing'
+      })
+  }
+  const note = {
+      content: body.content,
+      important: body.important || false
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  Note.findByIdAndUpdate(id, note, {new:true}).then(updated => {
+    response.json(updated)
+  }).catch(error => next(error))
+  
 })
 
 const unknownEndpoint = (request, response) => {
@@ -92,7 +101,19 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
